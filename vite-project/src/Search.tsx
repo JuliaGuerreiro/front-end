f5255b87b6bab504afb47ee3ae07e4931e75000e
JuliaGuerreiro <Juliamanu10@gmail.com>
2023-10-05 03:07:17 -0300

    Create search page

---
+++ vite-project/src/Search.tsx
@@ -0,0 +1,34 @@
+import React, { useState, ChangeEvent } from 'react';
+//import { useNavigate } from 'react-router-dom';
+//import api from './api'; // Import your API instance
+import { Header, Footer } from './Header-footer';
+//import './ImageUploadPage.css';
+//import './BackButton.css';
+import './Search.css';
+
+function SearchUser() {
+  const [textContent, setTextContent] = useState<string>('');
+
+  const handleTextContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextContent(e.target.value);
+  };
+
+  return (
+    <div>
+      <Header />
+      
+      <div id="search-user">
+                
+        <textarea 
+          placeholder="Search..."
+          value={textContent}
+          onChange={handleTextContentChange}
+        />
+ 
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default SearchUser;
