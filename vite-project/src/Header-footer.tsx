af3e0b4dd4626cde66669bc32fd8a091ab32ab7a
JuliaGuerreiro <Juliamanu10@gmail.com>
2023-10-05 03:08:58 -0300

    Incorporating icons into the header and footer

--- vite-project/src/Header-footer.tsx
+++ vite-project/src/Header-footer.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import './Header-footer.css';
 
 function Header() {
@@ -9,9 +10,54 @@ function Header() {
 }
 
 function Footer() {
+
+  const navigate = useNavigate();
+
+  const goToHomePage = () => {
+    navigate('/Teste');
+  };
+
+  const goToSearchPage = () => {
+    navigate('/Search');
+  };
+
+  const goToProfilePage = () => {
+    navigate('/Profile');
+  };
+
+  const goToAddPage = () => {
+    navigate('/UploadPage');
+  };
+
   return (
     <header className="fixed-footer">
-        <div></div>
+        <div className="icons">
+
+          <div className="home-button">
+            <a href="#" onClick={goToHomePage}>
+              <img src={"home.png"} alt="Home icon"/>
+            </a>
+          </div>
+
+          <div className="add-button">
+            <a href="#" onClick={goToAddPage}>
+              <img src={"add.png"} alt="Add icon"/>
+            </a>
+          </div>
+
+          <div className="search-button">
+            <a href="#" onClick={goToSearchPage}>
+              <img src={"search.png"} alt="Search icon"/>
+            </a>
+          </div>
+
+          <div className="profile-button">
+            <a href="#" onClick={goToProfilePage}>
+              <img src={"profile.png"} alt="Profile icon"/>
+            </a>
+          </div>
+
+        </div>
     </header>
   );
 }
